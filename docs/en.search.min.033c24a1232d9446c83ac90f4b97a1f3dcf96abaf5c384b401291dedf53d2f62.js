0cad6f4486aca71f3fd585e214b35a40b2fd2346
Marcin Misiak <wunderwaffel@gmail.com>
2020-05-03 23:40:56 +0200

    rebuild site

--- docs/en.search.min.033c24a1232d9446c83ac90f4b97a1f3dcf96abaf5c384b401291dedf53d2f62.js
+++ docs/en.search.min.033c24a1232d9446c83ac90f4b97a1f3dcf96abaf5c384b401291dedf53d2f62.js
@@ -3,7 +3,7 @@ input.addEventListener('focus',init);input.addEventListener('keyup',search);docu
 const characterPressed=String.fromCharCode(event.charCode);if(!isHotkey(characterPressed)){return;}
 input.focus();event.preventDefault();}
 function isHotkey(character){const dataHotkeys=input.getAttribute('data-hotkeys')||'';return dataHotkeys.indexOf(character)>=0;}
-function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/flexsearch.min.js');loadScript('/en.search-data.min.2aa31a2c473110b01b2e9b3a62cc9d19e4eb2da4b20f38fcf2618c702868dd60.js',function(){input.required=false;search();});}
+function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/flexsearch.min.js');loadScript('/en.search-data.min.d077857cf5e97427d539282abef78d550802fc0cc6a302082d89e8ccdea2da48.js',function(){input.required=false;search();});}
 function search(){while(results.firstChild){results.removeChild(results.firstChild);}
 if(!input.value){return;}
 const searchHits=window.bookSearchIndex.search(input.value,10);searchHits.forEach(function(page){const li=document.createElement('li'),a=li.appendChild(document.createElement('a'));a.href=page.href;a.textContent=page.title;results.appendChild(li);});}
